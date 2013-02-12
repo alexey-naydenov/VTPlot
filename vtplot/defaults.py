fe00be1e00120a793cb0d47c363a6850fbb1d638
Alexey Naydenov <alexey.naydenov@linux.com>
2013-02-12 10:19:02 +0400

    Minimal plugin without any functionality.

---
+++ vtplot/defaults.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# defaults.py
+#
+# Copyright (C) 2013 Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Some constants and default values for different things."""
+
+import os.path
+
+AUTHOR = 'Alexey Naydenov <alexey.naydenov@linux.com>'
+VERSION = '0.1'
+MODULE_NAME = 'vtplot'
+PLUGIN_CLASS = 'VTPlot'
+PLUGIN_NAME = 'vtplot'
+COMMENT = 'Plotting 1D and 2D graphs.'
+
+FOLDER = os.path.join(os.path.dirname(__file__))
---
+++ vtplot/vtplot.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# livability.py
+#
+# Copyright (C) 2013 Alexey Naydenov <alexey.naydenov@linux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Main plugin class."""
+
+import os
+import sys
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+from vitables import utils as vtutils
+from vitables import vtSite
+from vitables import plugin_utils
+
+from vitables.plugins.vtplot import defaults
+from vitables.plugins.vtplot import about_page
+
+__author__ = defaults.AUTHOR
+__version__ = defaults.VERSION
+# fields that must be defined: plugin_class, plugin_name, comment
+plugin_class = defaults.PLUGIN_CLASS
+plugin_name = defaults.PLUGIN_NAME
+comment = defaults.COMMENT
+
+def _(s):
+    return QtGui.QApplication.translate(defaults.MODULE_NAME, s)
+
+class VTPlot(QtCore.QObject):
+    """Main plugin class for all plotting stuff."""
+
+    def __init__(self):
+        super(VTPlot, self).__init__()
+        
+        self._vtgui = plugin_utils.getVTGui()
+        self._settings = QtCore.QSettings()
+        self._logger = plugin_utils.getLogger(defaults.MODULE_NAME)
+
+        self._add_submenu()
+
+    def helpAbout(self, parent):
+        self._about_page = about_page.AboutPage(parent)
+        return self._about_page
+
+    def _add_submenu(self):
+        """Add submenu for map actions."""
+        self._submenu = QtGui.QMenu(_('Plot'))
+        actions = {}
+        actions['nothing'] = QtGui.QAction(
+            _('Nothing'), self, shortcut=QtGui.QKeySequence.UnknownKey,
+            triggered=self._do_nothing, statusTip=_('Nothing'))
+        self._submenu.addAction(actions['nothing'])
+
+        plugin_utils.addToMenuBar(self._submenu)
+
+        plugin_utils.addToLeafContextMenu(actions.values())
+
+    def _do_nothing(self):
+        """Log a message."""
+        self._logger.debug('Doing nothing')
